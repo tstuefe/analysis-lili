0d36918e5b26ce7afc1f1eb6bc3f79e50a5910ec
tstuefe <thomas.stuefe@gmail.com>
2024-09-19 18:42:52 +0200

    xcxx

--- spring-petclinic/run-single-test.sh
+++ spring-petclinic/run-single-test.sh
@@ -27,11 +27,18 @@ function kill_process () {
 
 }
 
+# Start command $1
+# Basename for log $2
+function start_process () {
+	echo "Calling $1" >> $COMMANDS_LOG
+	$1 > "${DIR}/${2}.out" 2> "${DIR}/${2}.err" &
+	PID=$!
+} 
 
 ##### parameters
 
 # Test ID
-TESTNAME_DEFAULT="KANMWEG"
+TESTNAME_DEFAULT="KANNWEG"
 TESTNAME=${TESTNAME:-$TESTNAME_DEFAULT}
 
 # The JDK we test
@@ -60,15 +67,14 @@ FULL_GC_PERIOD_DEFAULT=""
 FULL_GC_PERIOD=${FULL_GC_PERIOD:-$FULL_GC_PERIOD_DEFAULT}
 
 # The jmeter script name
-JMETER_SCRIPT="./petclinic_test_plan-300secs.jmx"
+#JMETER_SCRIPT="./petclinic_test_plan-300secs.jmx"
+JMETER_SCRIPT="./petclinic_test_plan-short.jmx"
 
 # Number of seconds we wait for spring petclinic to finish startup
 STARTUPTIME=10
 
 
 ############# Initial tests ########################################
-
-echo "Are we root?"
 
 if [ `id -u` -ne 0 ] 
   then echo Please run this script as root or using sudo!
@@ -95,7 +101,7 @@ fi
 
 ########## Prepare test dir #########################################
 
-DIR="./result-${TESTNAME_BASE}"
+DIR="./result-${TESTNAME}"
 
 if [ -d "$DIR" ]; then
 	rm -rf "$DIR"
@@ -103,6 +109,8 @@ fi
 
 mkdir "$DIR"
 
+# contains all executed commands
+COMMANDS_LOG="${DIR}/commands.log"
 
 ########## Fork off Spring, perf (if needed) and the periodic jcmd
 ##########    runner (if needed) ###################################
@@ -110,30 +118,27 @@ mkdir "$DIR"
 echo "PID of start script $0 : $$"
 
 # Fork off spring
+# We run spring on CPUs 0..11
 TASKSET_SPRING="0xFFF"
 GCLOG="-Xlog:gc -Xlog:metaspace*"
-COMMAND="chrt -r 1 taskset ${TASKSET_SPRING}    ${JDK_TO_TEST}/bin/java $WHATGC $WHAT_MAX_HEAPSIZE $JVM_ARGS $GCLOG -jar ./spring-petclinic-2.5.0-SNAPSHOT.jar"
-echo "Calling: $COMMAND"
-${COMMAND} > "${DIR}/spring-out.txt" 2>"${DIR}/spring-err.txt" &
-SPRING_PID=$!
+COMMAND="chrt -r 1 taskset ${TASKSET_SPRING} ${JDK_TO_TEST}/bin/java $WHATGC $WHAT_MAX_HEAPSIZE $JVM_ARGS $GCLOG -jar ./spring-petclinic-2.5.0-SNAPSHOT.jar"
+start_process "$COMMAND" "spring-petclinic"
+SPRING_PID=$PID
 echo "PID of spring process: $SPRING_PID"
 
 # Fork off perf
 if [ "$PERF_COMMAND" != "" ]; then
 	COMMAND="$PERF_COMMAND -p ${SPRING_PID}"
-	echo "Calling: $COMMAND"
-	${COMMAND} > "${DIR}/perf-out.txt" 2>"${DIR}/perf-err.txt" &
-	PERF_PID=$!
-	echo "PID of perf: $PERF_PID"
+	start_process "$COMMAND" "perf"
+        PERF_PID=$PID
+	echo "PID of perf process: $PERF_PID"
 fi
-
 
 # Fork off jcmd for periodic full gc
 if [ "$FULL_GC_PERIOD" != "" ]; then
-	COMMAND="bash ./periodically-jcmd.sh"
-	echo "Calling: $COMMAND"
-	${COMMAND} > "${DIR}/jcmd-out.txt" 2>"${DIR}/jcmd-err.txt" &
-	JCMD_PID=$!
+	COMMAND="bash ./periodically-jcmd.sh ${FULL_GC_PERIOD} ${SPRING_PID}"
+	start_process "$COMMAND" "jcmd"
+	JCMD_PID=$PID
 	echo "PID of jcmd script: $JCMD_PID"
 fi
 
@@ -150,18 +155,44 @@ if [ "$PERF_COMMAND" != "" ]; then
 	exit_if_process_not_found $PERF_PID
 fi
 
-
 ########## Start jmeter and wait until its finished ###################################
 
-TASKSET_JMETER="0xF000"
-COMMAND="chrt -r 1 taskset ${TASKSET_JMETER}    ${JDK_TO_TEST}/bin/java -jar ./apache-jmeter-5.6.3/bin/ApacheJMeter.jar -n -H localhost -P 8080 -t ${JMETER_SCRIPT}"
-echo "Calling: $COMMAND"
-${COMMAND} > "${DIR}/jmeter-out.txt" 2>"${DIR}/jmeter-err.txt" 
+JMETER_OPTIONS="-H localhost -P 8080 -t ${JMETER_SCRIPT} -n -e -l ${DIR}/jmeter.jtl -o ${DIR}/jmeter-report"
 
+# We run jmeter on CPUs 12..15
+TASKSET_JMETER="0xF000"
+COMMAND="chrt -r 1 taskset ${TASKSET_JMETER} ${JDK_TO_TEST}/bin/java -jar ./apache-jmeter-5.6.3/bin/ApacheJMeter.jar ${JMETER_OPTIONS}"
+start_process "$COMMAND" "jmeter"
+JMETER_PID=$PID
+echo "PID of jmeter process: $JMETER_PID"
+
+# wait wait wait wait wait
+while kill -0 "$JMETER_PID" 2> /dev/null; do sleep 1; done;
+
+# big sigh - jmeter.log goes into the current dir, and its just simpler to move it to the
+# result dir manually
+if [ -f "./jmeter.log" ]; then
+	mv "./jmeter.log" "$DIR"
+fi
 
 # stop spring 
 
+# this should also kill perf and jcmd if they are running, but to be sure we kill them manually further down
+# (and hope pids did not get reused in that time)
+echo "Waiting for jmeter to finish..."
 kill_process $SPRING_PID
 
+if [ ! -z "${JCMD_PID:-}" ]; then
+	kill_process $JCMD_PID
+fi
 
+if [ ! -z "${PERF_PID:-}" ]; then
+	kill_process $PERF_PID
+fi
+
+sleep 2
+
+chown -R thomas .
+
+echo "Done."
 
